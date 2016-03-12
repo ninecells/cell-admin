58bdf9f96b369fd3930fbe9f7250f1f24c0833e6
koojunho <koojh@9cells.com>
2016-03-12 18:44:56 +0900

    Admin 용 콘텐츠는 lte 에서 제공하는 panel 안에서 그려지도록 표현

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -2,13 +2,24 @@
 
 @extends('ncells::admin.app')
 
-@section('page-title', 'Cells')
-@section('page-description', 'List of cells')
+@section('page-title', 'Dashboard')
+@section('page-description', 'Dashboard for admin users')
 
 @section('content')
-<ul>
-    @foreach($packages->getPackageList() as $pkg)
-    <li><a href="{{ $pkg->getUrl() }}">{{ $pkg->getName() }}</a></li>
-    @endforeach
-</ul>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Cells</h3>
+            </div>
+            <div class="box-body">
+                <ul>
+                    @foreach($packages->getPackageList() as $pkg)
+                    <li><a href="{{ $pkg->getUrl() }}">{{ $pkg->getName() }}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
